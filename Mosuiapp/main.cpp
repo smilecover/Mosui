1abd9786aacde85ab8ff9f8d0385933c04b105fc
smilecover <2997752539@qq.com>
2026-06-05 10:44:35 +0800

    资源文件维护更新

--- Mosuiapp/main.cpp
+++ Mosuiapp/main.cpp
@@ -5,25 +5,29 @@
 #include <QIcon>
 #include <QCoreApplication>
 #include <QDir>
+#ifdef HAS_QT_WEBENGINE
 #include <QtWebEngineQuick/qtwebenginequickglobal.h>
+#endif
 #include "Mosapp.h"
-// #include "Mostheme.h"
 
 
 int main(int argc, char *argv[]) {
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+#ifdef HAS_QT_WEBENGINE
     QtWebEngineQuick::initialize();
+#endif
     QGuiApplication app(argc, argv);
     QQuickStyle::setStyle("Fusion");
 
     QDir appDir(app.applicationDirPath());
-    app.setLibraryPaths(QStringList() << appDir.absolutePath() << app.libraryPaths().join(";"));
+    QStringList paths = app.libraryPaths();
+    paths.prepend(appDir.absolutePath());
+    app.setLibraryPaths(paths);
 
     QQmlApplicationEngine engine;
 
-    // 添加 QML 模块搜索路径
-    QString sharedPath = appDir.absoluteFilePath("shared");
-    engine.addImportPath(sharedPath);
+    // 添加 QML 模块搜索路径（appDir 即 shared/ 目录，QML 模块为其子目录）
+    engine.addImportPath(appDir.absolutePath());
 
     QIcon icon(":/logo.png");
     app.setWindowIcon(icon);
